c702817143aa891b44519490bfa89f313ea674d6
Ana Teresa Miller ter Kuile <Ana.Miller-ter-Kuile@nau.edu>
2023-05-02 14:37:26 -0700

    got a test model running for the JSDM

--- R/ana_models/jags_antecedent_jsdmodel.R
+++ R/ana_models/jags_antecedent_jsdmodel.R
@@ -5,12 +5,15 @@ model{
         # Data are around occupancy probability
         #this is "apparent" occupancy because it's not
         # corrected for detection
-        y[k,i] ~ dbern(psi[k,i])
+        y[k,i] ~ dnorm(eta[k,i], sig[k])
         
         #occupancy dependent on covariates
-        logit(psi[k,i]) <- a0[k] + 
-                            a1*TAnt[i] + 
-                            a2*PAnt[i]
+        #Probit model for occupancy as opposed to logistic
+        eta[k,i] <- a0[GroupID[k]] + 
+          a1[GroupID[k]]*TAnt[i] + 
+          a2[GroupID[k]]*PAnt[i] +
+          inprod(lv.coef[k,], LV[i,])
+        
         
         #could add in random effects and other covariates to above
         # if desired
@@ -19,41 +22,46 @@ model{
         # on grasses vs. forbes, large vs. small seeds, etc)
         
       } #n.sites
-
       
-  #Species-level Priors
-  a0[k] ~ dnorm(mu.a0, tau.a0)
-  a1[k] ~ dnorm(mu.tant, tau.tant)
-  a2[k] ~ dnorm(mu.ppt, tau.ppt)
-  
+      #new variance for model that allows residual variance
+      #to be correct
+      sig[k] <- (1/(1-sum(lv.coef[k,1:n.lv]^2)))
+
     } #n.species
+  
+  #Species-group level Priors
+  for(g in 1:n.groups){
+    a0[g] ~ dnorm(mu.a0, tau.a0)
+    a1[g] ~ dnorm(mu.tant, tau.tant)
+    a2[g] ~ dnorm(mu.ppt, tau.ppt)
+  }
   
   #Antecedent climate values
   for(i in 1:n.sites){
-    #antecedent climate value is the sum of all the 
+    #antecedent climate value is the sum of all the
     #weighted lagged values
     TAnt[i] <- sum(TmaxTemp[i,])
-    Pant[i] <- sum(PPTTemp[i,])
-    
+    PAnt[i] <- sum(PPTTemp[i,])
+
     #these are where the data you input into the model
     #come in for temp and ppt - here they're multiplied
-    #by the weights the model has given them for their 
+    #by the weights the model has given them for their
     #relative importance in driving the climate effect
     for(t in 1:n.lag){
       TmaxTemp[i,t] <- Tmax[i,t]*wA[t]
       PPTTemp[i,t] <- PPT[i,t]*wB[t]
     }
-    
+
   }
 
   #Antecedent climate priors
-  #sum of the weights for the climate lag, so we 
-  # can divide the total value by 1 
+  #sum of the weights for the climate lag, so we
+  # can divide the total value by 1
   sumA <- sum(deltaA[])
   sumB <- sum(deltaB[])
-  
+
   #Employing "delta trick" to give vector of weights dirichlet priors
-  #this is doing the dirichlet in two steps 
+  #this is doing the dirichlet in two steps
   #see Ogle et al. 2015 SAM model paper in Ecology Letters
   for(t in 1:n.lag){ #for total number of lags
    #the weigths for tmax - getting them to sum to 1
@@ -79,6 +87,54 @@ model{
   mu.ppt ~ dunif(-5,5)
   sd.ppt ~ dunif(0,5)
   tau.ppt <- pow(sd.ppt,-2)
+  
+  
+  #SETTING UP COVARIANCE MATRIX
+  #covariance matrix is going to be positive on the diagonal,
+  #zero on the topand between -1 and 1 correlatoin on the
+  # bottom 
+  #Latent variables
+  #Re-read these two papers to remind myself the 
+  #why of latent variable models
+  #they can be thought of as site-level variation
+  #https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecy.2754
+  #https://besjournals.onlinelibrary.wiley.com/doi/10.1111/2041-210X.12236
+  
+  for(j in 1:n.sites){
+    for(v in 1:(n.lv)){ #number of latent variables is species/2
+      #latent variable for site j and latent variable v
+      LV[j,v] ~ dnorm(0,1)
+    }
+  }
+  
+  #Latent variable coefficients with constranits
+  #diagonal elements positive, upper diagnoal zeros
+  for(v in 1:(n.lv-1)){
+    for(v2 in (v+1):n.lv){
+      #set the upper diagonal to 0
+      lv.coef[v, v2] <- 0
+    }
+  }
+  
+  #sign contraints on diagonal elements
+  for(v in 1:n.lv){
+    lv.coef[v,v] ~ dunif(0,1)
+  }
+  
+  #lower diagonal can be any value between total
+  #negative and total positive correlation
+  for(v in 2:n.lv){
+    for(v2 in 1:(v-1)){
+      lv.coef[v, v2] ~ dunif(-1,1)
+    }
+  }
+  
+  #other elements free
+  for(i in (n.lv+1):n.species){
+    for(v in 1:n.lv){
+      lv.coef[i,v] ~ dunif(-1,1) 
+    }
+  }
 
   }
   
---
+++ R/ana_models/run_antecedent_jsdmodel.R
@@ -0,0 +1,124 @@
+# Simulated data for Drake farm project
+# Ana Miller-ter Kuile
+# May 2, 2023
+
+#this script runs a model with simulated data
+#for the drake farm jsdm with antecedent climate
+
+# Load packages ---------------------------------------------------------------
+
+# Load packages, here and tidyverse for coding ease, 
+package.list <- c("here", "tidyverse", 
+                  "jagsUI",
+                  "R2jags", #jags wrapper
+                  "coda",
+                  "mcmcplots",
+                  "corrplot") 
+
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% 
+                                 installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
+
+# Load data list ----------------------------------------------------------
+
+
+n.species <- 20
+n.sites <- 5
+n.lag <- 3
+y <- matrix(rbinom(n.species*n.sites, 1, 0.5), n.species, n.sites)
+Tmax <- matrix(rnorm(n.sites*n.lag, mean = 0, sd  =1), n.sites, n.lag)
+#Tmax <- rnorm(n.sites)
+PPT <- matrix(rnorm(n.sites*n.lag, mean = 0, sd = 1), n.sites, n.lag)
+#PPT <- rnorm(n.sites)
+n.lv <- (n.species/2)
+GroupID <- c(rep(1, 5), rep(2, 5), rep(3, 5), rep(4, 5))
+n.groups = length(unique(GroupID))
+
+data_list <- list(n.species = n.species,
+                  n.sites = n.sites,
+                  n.lag = n.lag,
+                  n.groups= n.groups,
+                  y = y,
+                  Tmax = Tmax,
+                  PPT = PPT,
+                  n.lv = n.lv,
+                  GroupID = GroupID)
+
+
+
+# Parameters to track -----------------------------------------------------
+
+params <- c("lv.coef",
+            "a0",
+            "a1",
+            "a2",
+            "sig",
+            "wA",
+            'wB')
+
+# Run Model ---------------------------------------------------------------
+
+model <- here("R", "ana_models",
+              "jags_antecedent_jsdmodel.R")
+
+mod <- jagsUI::jags(data = data_list,
+                    inits = NULL,
+                    model.file = model,
+                    parameters.to.save = params,
+                    parallel = TRUE,
+                    n.chains = 3,
+                    n.iter = 4000,
+                    DIC = TRUE)
+
+
+#for convergence of parameters to track only
+params <- c("a0",
+            "a1",
+            "a2",
+            "wA",
+            'wB')
+
+mod2 <- jagsUI::jags(data = data_list,
+                    inits = NULL,
+                    model.file = model,
+                    parameters.to.save = params,
+                    parallel = TRUE,
+                    n.chains = 3,
+                    n.iter = 4000,
+                    DIC = TRUE)
+
+mcmcplot(mod2$samples)
+
+
+
+# Correlation matrix ------------------------------------------------------
+
+#calculate the correlation matrix from the latent variables
+#pull out the coefficients by species pair
+lv.coef<-mod$sims.list$lv.coef
+#make this an array of the number of coefficients,
+#number of species, number of species
+cmall<-array(NA,dim=c(dim(lv.coef)[1],n.species,n.species))
+#get the variance explained
+eps.res<-apply(lv.coef,c(1,2),function(x)1-sum(x^2))
+#get the per iteration value for the correlation
+for(i in 1:dim(lv.coef)[1]){ #for each mcmc sample
+  cmall[i,,]<-cov2cor(tcrossprod(lv.coef[i,,]) + diag(apply(eps.res,2,mean)))
+}
+#get the mean of that value to plot in the correlation matrix
+cmest<-apply(cmall,c(2,3),mean)
+
+#plot correlation matrix
+corplot<-cmest
+corrplot(corplot,method="circle",order="hclust",type="lower")
+#corrplot(corplot,method="color",order="original",type="lower",outline="black",tl.pos="d",tl.col="black")
+#corrplot(corplot,method="number",order="original",type="upper",col="black",addgrid.col="black", add=T,tl.pos="d",tl.col="black", cl.pos="n")
+#plot large number of species
+#corrplot(corplot,method="color",order="FPC",type="lower",outline="black",tl.pos="ld",tl.col="black",tl.cex=0.6,tl.srt = 45)
+
